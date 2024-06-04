803548b3cbf939f9a52fac65df7aea589740c370
VITOR HUGO PIRES DOS SANTOS <vitor.santos142@fatec.sp.gov.br>
2024-06-04 08:47:30 -0300

    Estrutura inicial das estilizações e variáveis

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Vitor Santos, Diógene Gabriel e Ingrid Norberto">
+    <title>Workout | Academia</title>
+    <link rel="stylesheet" href="./assets/css/index.css">
+</head>
+
+<body>
+    <h1>workout with me</h1>
+</body>
+
+</html>
