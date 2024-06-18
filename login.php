7bef69e4add41ac8a5332c2d58661b7d21e1713b
Diogene Gabriel <gabrieldiogene@gmail.com>
2024-06-18 09:54:38 -0300

    Puxando arquivos

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="./assets/css/login.css">
+</head>
+    <body>
+        <div class="container-imagem">
+            <img src="./assets/img/login.png" alt="">
+        </div>
+        <div class="container-login">
+            <div class="box">
+                <h1>Entrar</h1>
+                <p>Entre com suas credenciais inseridas na hora do registro.</P>
+                <form action="">
+                    <fieldset>
+                        <label for="email">Email:</label>
+                        <input type="text" name="email" id="email"/>
+                    </fieldset>
+                    <fieldset>
+                        <label for="senha">Senha:</label>
+                        <input type="password" name="senha" id="senha"/>
+                    </fieldset>  
+                    <button>Entrar em minha conta</button>
+                    <div class="formata-tag-esquerda">
+                        <a>Esqueceu sua senha?</a>
+                    </div>   
+                </form>
+                <span>Ainda não possui uma conta?</span>
+                <button>Criar uma conta</button>
+                <div class="footer">
+                    <span>Cookies & Privacidade</span>
+                    <span>Politicas Legais</span>
+                    <span>Workout@2024</span>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
