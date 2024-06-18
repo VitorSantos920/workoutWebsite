0daf1188ef8bd3308af806ac66f62fe8f08cae8e
Vitor Santos <pireshugo737@gmail.com>
2024-06-18 09:59:36 -0300

    Tela inicial (hero)

--- index.php
+++ index.php
@@ -12,7 +12,32 @@
 </head>
 
 <body>
-    <h1>workout with me</h1>
+
+    <nav>
+        <a href="#">workout</a>
+        <ul>
+            <li><a href="#">treinos</a></li>
+            <li><a href="#">dietas</a></li>
+            <li><a href="#">dietas</a></li>
+            <li><a href="#">vida saudavel</a></li>
+            <li><a href="#">comunidade</a></li>
+            <li><a href="cadatro.php">Cadastrar</a></li>
+            <li><a href="login.php">Entrar</a></li>
+    </nav>
+
+    <div class="section1">
+        <h1>Treine comigo</h1>
+        <p>Uma enorme seleção de conteúdo de saúde e fitness, receitas saudáveis ​​e histórias de transformação para ajudar você a ficar em forma e permanecer em forma!</p>
+        <button>Inscreva-se já</button>
+        <div class="brands">
+            <img src="assets/img/nike.svg" />
+            <img src="assets/img/buzzfeed.svg" />
+            <img src="assets/img/esprit.svg" />
+            <img src="assets/img/natgeo.svg" />
+            <img src="assets/img/dwff.svg" />
+            <img src="assets/img/huffpost.svg" />
+        </div>
+    </div>
 </body>
 
 </html>
